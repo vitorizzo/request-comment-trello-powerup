7f688ae869201d48174a3ab65d98646fd5a23e01
vitorizzo <vito.rizzo74@tim.it>
2025-05-27 21:40:12 +0200

    Aggiorno manifest e link popup

--- static/client.js
+++ static/client.js
@@ -2,18 +2,27 @@
 const t = TrelloPowerUp.iframe();
 
 /**
- * Questo viene invocato quando l’utente clicca su “Segnala guasto”
+ * Viene chiamato quando l’utente clicca su “Get Info”
+ * @param {Object} t    Trello iframe context
+ * @param {Object} opts Opzioni (non usate qui)
+ * @return {Promise}
  */
-function onCardButtonClick(t, options) {
-  // apri un popup con il tuo HTML di popup.html
-  return t.popup({
-    title: 'Segnala Guasto',
-    url: 'static/popup.html',
-    height: 200
-  });
+function onCardButtonClick(t, opts) {
+  // prendi l’ID della card corrente
+  return t.card('id')
+    .then(({ id }) => {
+      // apri un popup passando l’ID come argomento
+      return t.popup({
+        title: 'Get Info',
+        url: 'https://vitorizzo.github.io/request-comment-trello-powerup/static/popup.html',
+        // args verrà letta dentro popup.js
+        args: { cardId: id },
+        height: 200
+      });
+    });
 }
 
-// TrelloPowerUp.iframe() export
+// mappa le capability sui metodi
 window.TrelloPowerUp.initialize({
   'card-buttons': onCardButtonClick
 });

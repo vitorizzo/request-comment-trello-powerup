9bbf1464c1b19b33fc42012122335b3f30273365
vitorizzo <vito.rizzo74@tim.it>
2025-05-29 19:52:03 +0200

    fix struttura powerup

--- static/client.js
+++ static/client.js
@@ -1,28 +1,12 @@
 /* global TrelloPowerUp */
-const t = TrelloPowerUp.iframe();
-
-/**
- * Viene chiamato quando l’utente clicca su “Get Info”
- * @param {Object} t    Trello iframe context
- * @param {Object} opts Opzioni (non usate qui)
- * @return {Promise}
- */
-function onCardButtonClick(t, opts) {
-  // prendi l’ID della card corrente
-  return t.card('id')
-    .then(({ id }) => {
-      // apri un popup passando l’ID come argomento
-      return t.popup({
-        title: 'Get Info',
-        url: 'https://vitorizzo.github.io/request-comment-trello-powerup/static/popup.html',
-        // args verrà letta dentro popup.js
-        args: { cardId: id },
-        height: 200
-      });
+TrelloPowerUp.initialize({
+  'card-buttons': (t, opts) => {
+    // al click del bottone apri il popup
+    return t.popup({
+      title: 'Get Info',
+      url: 'https://vitorizzo.github.io/request-comment-trello-powerup/static/popup.html',
+      args: { /* se ti serve passare qualcosa */ },
+      height: 200
     });
-}
-
-// mappa le capability sui metodi
-window.TrelloPowerUp.initialize({
-  'card-buttons': onCardButtonClick
+  }
 });
